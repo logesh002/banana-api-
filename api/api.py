0303c7d9ac0fe92bc82d195493d16852da299bff
logesh002 <logeshgopu.g@gmail.com>
2022-11-07 14:55:55 +0530

    commit

---
+++ api/api.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn 
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import requests
+
+
+
+app=FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL =tf.keras.models.load_model("../models/1")
+ENDPOINT="http://localhost:8601/v1/models/banana:predict"
+CLASS_NAMES =['cordana', 'healthy', 'pestalotiopsis', 'sigatoka']
+
+def read_file_as_image(data)->np.array:
+    img_path=Image.open(BytesIO(data))
+    #img = image.load_img(BytesIO(data), target_size=(224, 224))
+    img = image.load_img(BytesIO(data), target_size=(256, 256))
+    img_array=image.img_to_array(img)
+    return img_array
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+
+@app.post("/predict")
+async def predict(file: UploadFile =File(...)):
+    image = read_file_as_image(await file.read())
+    image_batch=np.expand_dims(image,0)
+    #predictions=MODEL.predict(image_batch)
+    json_data = {
+        "instances":image_batch.tolist()
+        }
+    response=requests.post(ENDPOINT,json=json_data)
+    prediction = response.json()["predictions"][0]
+    
+    index=np.argmax(prediction)
+    predicted_class=CLASS_NAMES[index]
+    confidence=np.max(prediction) 
+    return {"prediction":predicted_class , "confidence":float(confidence)}
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost',port=8000)
