6c857312a3c69230a6861ff01f808fd111f1bbce
logesh002 <57947265+logesh002@users.noreply.github.com>
2022-11-22 20:31:29 +0530

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -16,6 +16,7 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "https://aquamarine-valkyrie-4f96dc.netlify.app",
+    "https://wonderful-wisp-52796a.netlify.app",
 ]
 app.add_middleware(
     CORSMiddleware,
